2f1fd4674ce200e376abf46be6b45a084334a4c2
VarunJoshi10 <varunjoshi12363@gmail.com>
2023-05-09 23:27:34 +0530

    added decks on landing page

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,19 +1,24 @@
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
-
+import cors from 'cors';
 import Deck from './models/Deck';
 
 import {config} from 'dotenv'
 config();
 
 const PORT=5000;
+
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
+app.get('/decks',async (req:Request,res:Response) => {
+    const decks=await Deck.find();
+    res.json(decks);
+})
 
 app.post('/decks', async(req: Request, res: Response) => {
-    console.log(req.body);
     const newDeck = new Deck({
         title: req.body.title,
     });

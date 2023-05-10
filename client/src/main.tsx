ad29b67048cd26750771426c9ff942beadae83b5
VarunJoshi10 <varunjoshi12363@gmail.com>
2023-05-10 23:22:25 +0530

    updated main,tsx

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css";
-import { createBrowserRouter, RouterProvider, Route } from "react-router-dom";
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
 import Deck from "./Deck";
 import { Header } from "./Header";
 
